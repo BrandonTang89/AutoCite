c4518a433105b6a8fa8f5386933f5595face4322
Brandon Tang <43487872+BrandonTang89@users.noreply.github.com>
2019-11-04 20:45:46 +0800

    Add files via upload

---
+++ AutoCite_CLI.py
@@ -0,0 +1,99 @@
+# Citation Machine
+import re, datetime, urllib.request
+from bs4 import BeautifulSoup
+from dateutil import parser
+'''
+web_address = "https://www.straitstimes.com/singapore/condo-conflicts"
+web_address = "https://www.channelnewsasia.com/news/asia/southeast-asian-leaders-meet-us-china-trade-war-asean-summit-12057538"
+web_address = "https://blog.seedly.sg/telegram-channels-that-every-millennials-singapore-must-have/"
+web_address = "https://stackoverflow.com/questions/5815747/beautifulsoup-getting-href"
+web_address = "https://www.channelnewsasia.com/news/asia/southeast-asian-leaders-meet-us-china-trade-war-asean-summit-12057538"
+web_address = "https://www.dissentmagazine.org/online_articles/can-democratic-socialism-rise-in-rural-america"
+'''
+
+def chicago_citation(web_address):
+    
+    req = urllib.request.Request(web_address, headers = {"User-Agent": 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.75.14 (KHTML, like Gecko) Version/7.0.3 Safari/7046A194A'})
+    response = urllib.request.urlopen(req)
+    html = response.read()
+
+    soup = BeautifulSoup(html, 'html.parser')
+
+    # Page Title from <title> tag | Website Title from <title> tag or from URL
+    title_text = str(soup.title.contents[0]) # String Slice Removes <title> and </title>
+    title_segments = title_text.split(" - ")
+    
+    if len(title_segments) > 1: # If website title coems after "-" in page title
+        website_title = title_segments[-1]
+        page_title = " - "
+        page_title = page_title.join(title_segments[:-1])
+
+    else:
+        website_title = re.search(r"([^.\/]+?)(?:\.(?:sg|net|com|org|gov|edu|int|eu|us))+",web_address).group(1) # Captures the last string between .DOMAIN and the . in front of that
+        website_title = website_title.capitalize() # Capitalises the first letter of the string
+        page_title = title_text
+
+
+    # Searches for Authors via href with "author"
+    for a in soup.find_all('a', href=True): # Find first author
+        if "author" in a['href']:
+            # print ("Author URL", a['href'])
+            author_path = a['href']
+            author_name = re.search(r"\/([^\/]+)$", author_path).group(1)
+            author_name = author_name.split("-")
+
+            break
+    try:
+        first_name = author_name[0].capitalize()
+        last_name = author_name[-1].capitalize()
+    except:
+        first_name = last_name = ""
+
+    # Accessed Date
+    today = datetime.date.today()
+    date_accessed = today.strftime("%d %b %Y")
+
+    # Published Date
+    try:
+        date_published = ""
+        time_element = soup.find('time')
+        
+        if time_element.has_attr('datetime') and time_element["datetime"]!= "": # Take date from attribute
+            web_datetime = time_element["datetime"]
+            date = parser.parse(web_datetime)
+            
+        else: # take date from contents
+            date_published = time_element.contents[0]
+            date = parser.parse(date_published)
+
+        date_published = date.strftime("%d %b %Y")
+            
+
+    except Exception as e:
+        #print(e)
+        date_published = ""
+        pass
+
+
+    # Compiling the Citation
+    if first_name != "" and last_name != "":
+        citation = last_name + ", " + first_name + ". "
+    else:
+        citation = ""
+
+    citation += '"' +page_title + '." ' + website_title + ", "
+    if date_published != "":
+        citation += date_published + ", "
+
+    citation += web_address+ ". (retrieved " + date_accessed + ")."
+
+    return citation
+
+
+while True:
+    web_address = input()
+    try:
+        print(chicago_citation(web_address))
+    except:
+        print("Failed to make Citation :(")
+
